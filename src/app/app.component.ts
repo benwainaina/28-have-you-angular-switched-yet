b819ebadd8c4d5ef3165af89a145813a9143d572
ben wainaina <bwainainamwangi@gmail.com>
2024-12-02 19:16:04 +0300

    feat: initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+
+type TCardFace = 'front' | 'back';
+type TCardMode = 'basic' | 'student' | 'advanced' | 'premium';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgSwitch, NgSwitchCase, NgSwitchDefault, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = '28-have-you-angular-switched-yet';
+  public activeFace: TCardFace = 'front';
+  public selectedBackContent: FormControl<TCardMode | null> =
+    new FormControl<TCardMode>('premium');
+
+  public onCardFaceChange(face: TCardFace): void {
+    this.activeFace = face;
+  }
 }
