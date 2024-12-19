b4755479b1b6a3277577e4cdb405f8bcb295a5d8
ben wainaina <bwainainamwangi@gmail.com>
2024-12-19 19:16:32 +0300

    cleanup: remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,16 +15,6 @@ export class AppComponent {
   public selectedBackContent: FormControl<TCardMode | null> =
     new FormControl<TCardMode>('premium');
 
-  // ngOnInit() {
-  //   const evaluation = Math.random() * 10 > 1;
-  //   switch (evaluation) {
-  //     case true:
-  //       console.log('true');
-  //     case false:
-  //       console.log('false');
-  //   }
-  // }
-
   public onCardFaceChange(face: TCardFace): void {
     this.activeFace = face;
   }
